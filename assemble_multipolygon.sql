5a590a1c9471e8f4b32f7f5a8d81b5b5637f3ebd
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2014-09-21 03:39:19 +0200

    Bugfixes, ...

--- assemble_multipolygon.sql
+++ assemble_multipolygon.sql
@@ -25,7 +25,7 @@ BEGIN
   end if;
 
   -- tags
-  tags:=(select tags from relations where relations.id=id);
+  tags:=(select relations.tags from relations where relations.id=id);
 
   -- check if type is correct
   if tags->'type' not in ('multipolygon', 'boundary') then
@@ -35,7 +35,7 @@ BEGIN
 
   -- generate multipolygon geometry
   geom:=build_multipolygon(
-    (select array_agg((select linestring from ways where ways.id=outer_members[i])) from generate_series(1, array_upper(outer_members, 1)) i),
+    (select array_agg((select linestring from ways where ways.id=outer_id)) from unnest(outer_members) outer_id),
     (select array_agg((select linestring from ways where ways.id=member_id)) from relation_members where relation_id=id and member_type='W' and member_role in ('inner', 'enclave') group by relation_id));
 
   -- of geometry is not valid, then return false
@@ -45,9 +45,9 @@ BEGIN
 
   -- check if all outer polygons are equal
   outer_equal=true;
-  outer_tags=way_assemble_tags(outer_members[1]);
+  outer_tags=(select ways.tags from ways where ways.id=outer_members[1]);
   for i in 2..array_upper(outer_members, 1) loop
-    if way_assemble_tags(outer_members[i])!=outer_tags then
+    if (select ways.tags from ways where ways.id=outer_members[i])!=outer_tags then
       outer_equal=false;
     end if;
   end loop;
@@ -66,11 +66,8 @@ BEGIN
       return false;
     end if;
     
-    -- else use tags from outer polygon(s) and delete from multipolygons
-    tags:=tags_merge(tags, way_assemble_tags(outer_members[1]));
-    for i in 1..array_upper(outer_members, 1) loop
-      delete from multipolygons where multipolygons.id='W'||(outer_members[i]);
-    end loop;
+    -- else use tags from outer polygon(s)
+    tags:=tags_merge(tags, outer_tags);
   end if;
 
   -- if no tags (beside 'type'), return
@@ -79,9 +76,6 @@ BEGIN
   end if;
 
   -- raise notice 'assemble_multipolygon(%)', id;
-
-  -- get members
-  select array_agg(member_type || member_id) as ids, array_agg(member_role) as roles into members from relation_members where relation_id=id group by relation_id;
 
   -- okay, insert
   insert into multipolygons
