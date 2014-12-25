314272ee196d7ca161b2feef53d7757f4764fb98
Peter Kunath <kunath@unary.de>
2014-12-25 08:13:36 +0100

    Better handling of non-closed geometries
    - don't loose lines with 3 points or less

--- make_multipolygon.sql
+++ make_multipolygon.sql
@@ -8,6 +8,9 @@ declare
   cur		geometry;
   cur1		geometry;
 begin
+  -- in contrast to simply calling ST_Collect/ST_Union, 
+  -- this function generates closed geometries only (otherwise null is returned)
+  todo:=Array[]::geometry[];
   done:=Array[]::geometry[];
 
   -- empty array
@@ -19,41 +22,46 @@ begin
   for i in array_lower(src, 1)..array_upper(src, 1) loop
     if src[i] is null then
       -- raise notice 'got null geometry, index %', i;
-    elsif ST_NPoints(src[i])>3 then
-      if (ST_IsClosed(src[i])) then
+    elsif not ST_IsValid(src[i]) then
+      raise notice 'ignore invalid line %', i;
+    elsif ST_IsClosed(src[i]) then
+      if ST_NPoints(src[i])>3 then
         done:=array_append(done, ST_MakePolygon(src[i]));
-      elsif not ST_IsValid(src[i]) then
-        raise notice 'ignore invalid line %', i;
       else
-        todo:=array_append(todo, src[i]);
+        raise notice 'ignore degenerated area %', i;
       end if;
+    else
+      todo:=array_append(todo, src[i]);
     end if;
   end loop;
 
-  -- merge all other geometries together
-  begin
-    cur:=ST_LineMerge(ST_GeomFromEWKT(ST_AsEWKT(ST_Collect(todo))));
-  exception when others then
-    raise notice 'error merging lines';
-    return null;
-  end;
-
-  -- if those build a closed geometry
-  if ST_NumGeometries(cur) is null then
-    if ST_IsClosed(cur) and ST_NPoints(cur)>3 then
-      done:=array_append(done, ST_MakePolygon(cur));
-    end if;
-  else
-  -- several geometries? check each of them ...
+  -- geometries in the todo list?
+  if array_length(todo, 1) is not null then
+    -- merge all other geometries together
+    begin
+      cur:=ST_LineMerge(ST_GeomFromEWKT(ST_AsEWKT(ST_Collect(todo))));
+      exception when others then
+        raise notice 'error merging lines';
+        return null;
+    end;
+    -- check each geometry whether it is closed
     for i in 1..ST_NumGeometries(cur) loop
       cur1:=ST_GeometryN(cur, i);
-      if ST_IsClosed(cur1) and ST_NPoints(cur1)>3 then
-	done:=array_append(done, ST_MakePolygon(cur1));
+      if ST_IsClosed(cur1) then
+	if ST_NPoints(cur1)>3 then
+	  done:=array_append(done, ST_MakePolygon(cur1));
+        else
+          raise notice 'ignore degenerated area';
+        end if;
       end if;
     end loop;
   end if;
 
   -- we are done :)
-  return ST_Collect(done);
+  if array_length(done, 1) is not null then
+    return ST_Collect(done);
+  else
+    return null;
+  end if;
 end;
 $$ language 'plpgsql';
